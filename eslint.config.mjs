b5a3a3b8c4bb8262b0434668a296446436e706e3
mallowlabs <mallowlabs@gmail.com>
2026-02-21 09:40:42 +0900

    Update ESLint configuration for v10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
+import { defineConfig } from 'eslint/config';
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default [
+export default defineConfig([
   js.configs.recommended,
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
-];
+]);
